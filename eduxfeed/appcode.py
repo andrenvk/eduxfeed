1267740f9bbe898e73820d284c2796874ea4fb60
Ondrej Novak <novako20@fit.cvut.cz>
2017-02-05 17:10:14 +0100

    user settings

--- eduxfeed/appcode.py
+++ eduxfeed/appcode.py
@@ -1,6 +1,7 @@
 from . import db
 from .api import user_enrolled
 
+import re
 import uuid
 import hashlib
 
@@ -36,14 +37,82 @@ def user_register(username):
 
     config['COURSES'] = {}
     cfg = config['COURSES']
-    courses = user_enrolled(username)
-    courses_all = db.edux_courses()
+    courses = user_enrolled(username, session=None)
+    courses_all = db.edux_pages()['COURSES']
     for course in courses:
         if course in courses_all:
             cfg[course] = courses_all[course]
 
     db.user_config_set(username, config)
     db.user_feed_set(username, feed={})
+
+
+def user_settings(username):
+    user = db.user_config(username)
+
+    config = {}
+    config['media'] = user['FEED'].getboolean('media', fallback=True)
+    config['en'] = user['FEED'].getboolean('en', fallback=False)
+
+    courses = list(user['COURSES'])
+    courses_all = list(db.edux_pages()['COURSES'])
+
+    return config, courses, courses_all
+
+
+def user_update(username, config, courses):
+    user = db.user_config(username)
+    feed = db.user_feed(username)
+
+    if 'media' in config:
+        user['FEED']['media'] = str(int(True))
+    elif user['FEED'].getboolean('media', fallback=True):
+        # 'media' disabled
+        user['FEED']['media'] = str(int(False))
+        try:
+            del feed['media']
+        except:
+            pass
+
+    if 'en' in config:
+        user['FEED']['en'] = str(int(True))
+    elif user['FEED'].getboolean('en', fallback=False):
+        # 'en' disabled
+        user['FEED']['en'] = str(int(False))
+        for src in ('pages', 'media'):
+            if src in feed:
+                delete = {}
+                for course in feed[src]:
+                    delete[course] = []
+                    for path in feed[src][course]:
+                        if re.match('[^/]+(/_media)?/en/', path):
+                            delete[course].append(path)
+                for course in delete:
+                    for key in delete[course]:
+                        del feed[src][course][key]
+                    if not feed[src][course]:
+                        del feed[src][course]
+
+    # deleted courses
+    for course in user['COURSES']:
+        if course not in courses:
+            del user['COURSES'][course]
+            for src in ('pages', 'media'):
+                if src in feed and course in feed[src]:
+                    del feed[src][course]
+
+    for src in ('pages', 'media'):
+        if src in feed and not feed[src]:
+            del feed[src]
+
+    # added courses
+    courses_all = db.edux_pages()['COURSES']
+    for course in courses:
+        if course not in user['COURSES']:
+            user['COURSES'][course] = courses_all[course]
+
+    db.user_feed_set(username, feed)
+    db.user_config_set(username, user)
 
 
 def item_hash(username, args, secret=None):
